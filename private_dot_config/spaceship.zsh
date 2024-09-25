8ace0946df2b091fdda5244aa67fcc3cf1e3ff7a
Yax <1949284+kianby@users.noreply.github.com>
2024-09-25 17:48:14 +0200

    update spaceship config

--- private_dot_config/spaceship.zsh
+++ private_dot_config/spaceship.zsh
@@ -2,7 +2,9 @@
 SPACESHIP_TIME_SHOW=false
 
 # Display username always
-SPACESHIP_USER_SHOW=never
+SPACESHIP_USER_SHOW=false
 
 # Do not truncate path in repos
 SPACESHIP_DIR_TRUNC_REPO=false
+
+SPACESHIP_JOBS_SHOW=false

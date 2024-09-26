b64e2d49d4c9f77e24c102b97bb994bd4aeed22e
Yax <1949284+kianby@users.noreply.github.com>
2024-09-26 15:04:29 +0200

    spaceship config

--- private_dot_config/spaceship.zsh
+++ private_dot_config/spaceship.zsh
@@ -1,10 +1,15 @@
-# Display time
+# Never display time
 SPACESHIP_TIME_SHOW=false
 
-# Display username always
+# Never display user
 SPACESHIP_USER_SHOW=false
 
 # Do not truncate path in repos
 SPACESHIP_DIR_TRUNC_REPO=false
 
+# Never display jobs
 SPACESHIP_JOBS_SHOW=false
+
+# Async display git branch
+# and apply workaround in spaceship code: https://github.com/spaceship-prompt/spaceship-prompt/issues/1193
+SPACESHIP_GIT_BRANCH_ASYNC=true
